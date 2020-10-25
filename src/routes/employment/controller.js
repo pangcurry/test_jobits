94fa00cd20e8b00d697489494e3b66fdffab6524
pangcurry <magicshuffle2424@hotmail.com>
2020-10-26 08:58:46 +0900

    [REFACTOR] Move logic from controller.js to services/employment.js

--- src/routes/employment/controller.js
+++ src/routes/employment/controller.js
@@ -1,68 +1,58 @@
 const { Employment } = require('../../models');
+const Employments = require('../../services/employment');
+
+const employment = new Employments(Employment);
+const MAX_LIMIT = 6;
 
 const list = async (req,res,next) => {
-  let result = [];
-  const { page } = req.params;
+  const page = parseInt(req.params.page);
   try {
-    const list = await Employment.findAll({
-      attributes: ['id','question','answer'],
-      order: [['createdAt','asc']],
-      limit: 10,
-      offset: (page-1)*10,
-    });
-
-    res.status(200).json({  // list 가져오기 성공
-      lists: list
-    });
+    const lists = await employment.getEmploymentList(page,MAX_LIMIT);
+    res.status(200).json({ lists:lists });// list 가져오기 성공
   }
   catch(err) {
-    console.log(err.message);
-    res.status(500).end();// list 가져오기 안됨
+    res.status(err.status).send({
+      message: err.message
+    })// list 가져오기 안됨
   }
 }
 
 const write = async (req,res,next) => {
-  console.log("in write");
+
   const { question,answer } = req.body;
   try {
-    console.log("try in");
-    await Employment.create({
-      question,
-      answer,
-    });
+    await employment.write(question, answer);
     res.status(200).end();//등록성공
 
   } catch (err) {
-    res.status(500).end();// 등록 안됨
+    res.status(err.status).send({
+      message: err.message
+    });// 등록 안됨
   }
 }
 
 const modify = async (req,res,next) => {
   const { question,answer } = req.body;
-  const { id } = req.params;
+  const id = parseInt(req.params.id);
   try {
-    await Employment.update({
-      question,
-      answer
-    },{
-      where: { id }
-    });
+    await employment.modify(id,question,answer);
     res.status(200).end();// 수정 성공
   } catch (err) {
-    res.status(500).end();// 수정 안됨
+    res.status(err.status).send({
+      message: err.message
+    });// 수정 안됨
   }
 }
 
 const drop = async (req,res,next) => {
-  const { id } = req.params;
-  if(!id) { res.status(400).end(); }
+  const id = parseInt(req.params.id);
   try {
-    const result = await Employment.destroy({
-      where: { id }
-    });
+    await employment.drop(id);
     res.status(200).end();//삭제 성공
   } catch (err) {
-    res.status(500).end();//삭제 실패
+    res.status(err.status).send({
+      message: err.message
+    });//삭제 실패
   }
 }
 
