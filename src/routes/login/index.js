29328609a5780dcc0b88d4745c51aaa795c5fab6
pangcurry <magicshuffle2424@hotmail.com>
2020-10-26 08:50:31 +0900

    [Fix] Deactivation

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -3,7 +3,7 @@ const controller = require('./controller');
 const auth = require('../../middlewares/auth').authVerify;
 
 router.post('/',controller.login);
-router.post('/refresh',auth, controller.refresh);
+//router.post('/refresh',auth, controller.refresh);
 router.put('/password',auth, controller.password);
 
 module.exports = router;
